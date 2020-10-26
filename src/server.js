80b7ffed10f7d0433e33758778bbaceaaf3a813e
Franciele <goulartfra@gmail.com>
2020-10-26 00:37:10 -0300

    pag2

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log('Hoyen Turupom?')
+// importar dependencia 
+const express = require('express');
+const path = require('path');
+
+// iniciando o express
+const server = express();
+
+server
+// utilizando os arquivos estáticos
+.use(express.static ('public'))
+// criar uma rota
+.get('/', (request, response) =>  {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+// ligar o servidor
+server.listen(5500)
