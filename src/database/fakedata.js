b240a0203e9df3423c89194b567b90b82802bfcf
Franciele <goulartfra@gmail.com>
2020-10-27 01:16:55 -0300

    pag2

---
+++ src/database/fakedata.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+
+        id: 1,
+        lat: -30.0415789,
+        lng: -51.1962634,
+        name: "Lar das meninas"
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    },
+    {
+        id: 2,
+        name: "Lar dos meninos"
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1565535941810-e9892acd7704?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            
+            "https://images.unsplash.com/photo-1545013806-8e1d077550ca?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+
+        ],
+
+
+    },
+]
