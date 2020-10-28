3a651ab7ffe7487733f93fd2619e9c7efc9ae694
Franciele <goulartfra@gmail.com>
2020-10-28 00:43:01 -0300

    pag2

--- src/database/db.js
+++ src/database/db.js
@@ -1,3 +1,20 @@
 const Database =require('sqlite-async');
 
-Database.open(__dirname + '/database.sqlite');
+Database.open(__dirname + '/database.sqlite').then(execute);
+
+function execute(db) {
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS orphanages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        lat TEXT, 
+        lng TEXT,
+        name TEXT,
+        about TEXT, 
+        whatsapp TEXT, 
+        images TEXT,
+        instructions TEXT,
+        opening_hours TEXT,
+        open_on_weekends TEXT
+    );
+    `)
+}
